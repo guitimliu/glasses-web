9aba59a0d05145af646f8810205abbc35f9113af
Gui <guitimliu@gmail.com>
2022-09-17 09:44:56 +0800

    調整 Header & Footer icon 與麵包選單

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,11 @@
 $('.header__menu-button').click(() => {
-  $('.header__menu').toggle();
+  $('.header__menu').slideToggle();
+  $('.header__menu-button').toggle();
+  $('.header__menu-button-close').toggle();
+});
+
+$('.header__menu-button-close').click(() => {
+  $('.header__menu').slideToggle();
+  $('.header__menu-button').toggle();
+  $('.header__menu-button-close').toggle();
 });
