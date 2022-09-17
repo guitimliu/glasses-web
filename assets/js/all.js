d31bfcef00a1b1d6da6df138d3d6aa6e0133f039
Gui <guitimliu@gmail.com>
2022-09-17 09:42:24 +0800

    Update 2022-09-17T01:42:22.628Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,13 @@
 "use strict";
 
 $('.header__menu-button').click(function () {
-  $('.header__menu').toggle();
+  $('.header__menu').slideToggle();
+  $('.header__menu-button').toggle();
+  $('.header__menu-button-close').toggle();
+});
+$('.header__menu-button-close').click(function () {
+  $('.header__menu').slideToggle();
+  $('.header__menu-button').toggle();
+  $('.header__menu-button-close').toggle();
 });
 //# sourceMappingURL=all.js.map
